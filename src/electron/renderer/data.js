4e199490d5761c9f52ae5a5cec12d57877a69521
Richard <liujh88888@sina.cn>
2019-06-27 21:52:25 +0800

    DataManager  refactor

--- src/electron/renderer/data.js
+++ src/electron/renderer/data.js
@@ -13,16 +13,40 @@ export class DataManager {
     return this.dP.writeData(dataType, Date.now(), obj)
   }
 
-  async getData (dataType = DataType.SchulteTable) {
+  async getData (dataType = DataType.SchulteTable, callback) {
     let data = await this.dP.readData()
     data = data[dataType]
-    let res = []
-    let index = 0
-    for (const key in data) {
-      if (index > 4) break
-      res.push(data[key])
-      index++
-    }
-    return res
+    return callback(data)
+  }
+
+  async getSchultTableLastFive () {
+    return this.getData(DataType.SchulteTable, (data) => {
+      let res = []
+      let keys = Object.keys(data)
+
+      // read last 5 object
+      for (let i = 1; i <= 5; i++) {
+        res.push(data[keys[keys.length - i]])
+      }
+
+      return res
+    })
+  }
+
+  async getSchultTableToday () {
+    return this.getData(DataType.SchulteTable, (data) => {
+      let toDayStartTimestamp = new Date().setHours(0, 0, 0, 0)
+      let toDayEndTimestamp = new Date().setHours(23, 59, 59, 999)
+      let res = []
+
+      for (const key in data) {
+        if (parseInt(key) >= toDayStartTimestamp &&
+          parseInt(key) <= toDayEndTimestamp) {
+          res.push(data[key])
+        }
+      }
+
+      return res
+    })
   }
 }
