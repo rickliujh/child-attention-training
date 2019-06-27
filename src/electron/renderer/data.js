aca08b7c5f9dd3500f4d075cc71d0b5907e14599
Richard <liujh88888@sina.cn>
2019-06-27 21:07:25 +0800

    updata DataManager & DataPersistence

--- src/electron/renderer/data.js
+++ src/electron/renderer/data.js
@@ -1,18 +1,28 @@
-import DP from './persistence'
+import { DataPersistence } from './persistence'
+
+export const DataType = {
+  SchulteTable: 'SchulteTable'
+}
 
 export class DataManager {
   constructor () {
-    this.dP = DP
+    this.dP = new DataPersistence()
   }
 
-  async saveData (obj = {}) {
-    return this.dP.writeData(Date.now, obj)
+  async saveData (dataType = DataType.SchulteTable, obj = {}) {
+    return this.dP.writeData(dataType, Date.now(), obj)
   }
 
-  async readData () {
+  async getData (dataType = DataType.SchulteTable) {
     let data = await this.dP.readData()
+    data = data[dataType]
+    let res = []
+    let index = 0
     for (const key in data) {
-      return data[key]
+      if (index > 4) break
+      res.push(data[key])
+      index++
     }
+    return res
   }
 }
--- src/electron/renderer/persistence.js
+++ src/electron/renderer/persistence.js
@@ -4,8 +4,8 @@ import { remote } from 'electron'
 
 const dataPath = path.join(remote.app.getAppPath(), '/data.json')
 
-export default {
-  writeFile: async function (data) {
+export class DataPersistence {
+  async writeFile (data) {
     let json = JSON.stringify(data, null, 2)
     return new Promise((resolve, reject) => {
       fs.writeFile(dataPath, json, { encoding: 'utf8' }, (err) => {
@@ -13,27 +13,32 @@ export default {
         resolve(true)
       })
     })
-  },
+  }
 
-  readData: async function () {
+  async readData () {
     return new Promise((resolve, reject) => {
       fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
         if (err) reject(err)
         resolve(JSON.parse(data))
       })
     })
-  },
+  }
 
-  writeData: async function (key, data) {
+  async writeData (dataType, key, data) {
     return new Promise(async (resolve, reject) => {
       let newData = {}
-      if (fs.existsSync(dataPath)) {
-        newData = await this.readData()
-      }
-      newData[key] = data
+
+      if (fs.existsSync(dataPath)) { newData = await this.readData() }
+      if (newData[dataType] === undefined) { newData[dataType] = {} }
+
+      newData[dataType][key] = data
       this.writeFile(newData)
         .then(val => resolve(val))
         .catch(err => reject(err))
     })
   }
+}
+
+export default {
+  DataPersistence: DataPersistence
 }
