f02a4764ed6cb58aa293da95d8e5caa8cb8d0f36
Richard <liujh88888@sina.cn>
2019-06-27 21:08:55 +0800

    add sava training function

--- src/views/training/pages/SchulteGrid.vue
+++ src/views/training/pages/SchulteGrid.vue
@@ -8,13 +8,30 @@
         @grid-count-change="handleGridCountChange"
         @start="handleStart"
         @reset="handleReset"
+        @total-time="handleTotalTime"
       ></control-panel>
     </q-scroll-area>
+
+    <q-dialog v-model="dialog" seamless position="right">
+      <q-card style="width: 350px">
+        <q-card-section class="row items-center no-wrap">
+          <div>
+            <div class="text-weight-bold">数据已保存</div>
+            <div class="text-grey">耗时：{{totalTime}}s  错误：{{wrongNum}}次</div>
+          </div>
+
+          <q-space />
+
+          <q-btn flat round icon="close" v-close-popup />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
 <script>
 import _ from 'underscore'
+import { DataManager, DataType } from '../../../electron/renderer/data'
 import ControlPanel from '../components/ControlPanel'
 import GridView from '../components/GridView'
 export default {
@@ -28,7 +45,10 @@ export default {
       grids: this.getGrids(25),
       nextValue: 1,
       isRight: false,
-      isDone: true
+      isDone: true,
+      wrongNum: 0,
+      totalTime: 0,
+      dialog: false
     }
   },
   methods: {
@@ -56,6 +76,7 @@ export default {
       this.shuffle()
       this.nextValue = 1
       this.isDone = true
+      this.wrongNum = 0
     },
     handleGridClick (value) {
       if (this.isDone === true) {
@@ -70,7 +91,26 @@ export default {
         this.nextValue++
       } else {
         this.isRight = false
+        this.wrongNum++
       }
+    },
+    handleTotalTime (time) {
+      this.totalTime = time
+      this.saveTrainingData()
+    },
+    showDialog () {
+      this.dialog = true
+      setTimeout(() => {
+        this.dialog = false
+      }, 3000)
+    },
+    saveTrainingData () {
+      const DM = new DataManager()
+      DM.saveData(DataType.SchulteTable, {
+        time: this.totalTime,
+        wrong: this.wrongNum
+      })
+      this.showDialog()
     }
   },
   mounted () {
